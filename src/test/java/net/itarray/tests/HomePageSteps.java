c2e8704b5b60efffd36a359ab7c6e5f3b4e96e99
dzaiats <denys.zaiats@gmail.com>
2017-09-08 11:46:47 +0200

    [master] - updated an example

--- src/test/java/net/itarray/tests/HomePageSteps.java
+++ src/test/java/net/itarray/tests/HomePageSteps.java
@@ -3,7 +3,10 @@ package net.itarray.tests;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
-import http.helpers.EnvironmentHelper;
+import net.itarray.automotion.tools.driver.DriverHelper;
+import net.itarray.automotion.tools.driver.WebDriverFactory;
+import net.itarray.automotion.tools.helpers.EnvironmentHelper;
+import net.itarray.automotion.validation.ResponsiveUIValidator;
 import net.itarray.pages.HomePage;
 import org.assertj.core.api.SoftAssertions;
 import org.junit.Assert;
@@ -11,17 +14,17 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import util.driver.DriverHelper;
-import util.driver.WebDriverFactory;
-import util.validator.ResponsiveUIValidator;
 
 import java.awt.*;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
-import static environment.EnvironmentFactory.isChrome;
-import static environment.EnvironmentFactory.isRemote;
+import static net.itarray.automotion.internal.properties.PercentReference.PAGE;
+import static net.itarray.automotion.tools.environment.EnvironmentFactory.isChrome;
+import static net.itarray.automotion.tools.environment.EnvironmentFactory.isRemote;
+import static net.itarray.automotion.validation.properties.Condition.between;
+import static net.itarray.automotion.validation.properties.Expression.percent;
 
 public class HomePageSteps {
     private static WebDriver driver;
@@ -30,7 +33,7 @@ public class HomePageSteps {
     private WebDriverFactory driverFactory;
 
     private void initPage() {
-        driver.manage().window().maximize();
+        //driver.manage().window().maximize();
         page = new HomePage(driver);
         uiValidator = new ResponsiveUIValidator(driver);
         uiValidator.setLinesColor(Color.BLACK);
@@ -71,13 +74,11 @@ public class HomePageSteps {
 
     @Then("^top slider element has correct alignment and width between (\\d+) and (\\d+) px$")
     public void top_slider_element_has_correct_alignment(int minWidth, int maxWidth) {
-        boolean success = uiValidator.init("Validation of Top Slider Element")
+        boolean success = uiValidator.snapshot("Validation of Top Slider Element")
                 .findElement(page.topSlider(), "Top Slider")
-                .sameOffsetLeftAs(page.gridContainer(), "Grid Container")
-                .sameOffsetBottomAs(page.topTextBlock(), "Text Block")
-                .changeMetricsUnitsTo(ResponsiveUIValidator.Units.PX)
-                .widthBetween(minWidth, maxWidth)
-                .drawMap()
+                .isLeftAlignedWith(page.gridContainer(), "Grid Container")
+                .isBottomAlignedWith(page.topTextBlock(), "Text Block")
+                .hasWidth(between(percent(minWidth, PAGE)).and(percent(maxWidth, PAGE)))
                 .validate();
 
         Assert.assertTrue("Visual validation of Top Slider is failed", success);
@@ -85,11 +86,10 @@ public class HomePageSteps {
 
     @Then("^top text block element has correct alignment$")
     public void top_text_block_element_has_correct_alignment() {
-        boolean success = uiValidator.init("Validation of Top Text block")
+        boolean success = uiValidator.snapshot("Validation of Top Text block")
                 .findElement(page.topTextBlock(), "Top Text block")
-                .sameOffsetRightAs(page.gridContainer(), "Grid Container")
-                .sameOffsetTopAs(page.topSlider(), "Top Slider")
-                .drawMap()
+                .isRightAlignedWith(page.gridContainer(), "Grid Container")
+                .isTopAlignedWith(page.topSlider(), "Top Slider")
                 .validate();
 
         Assert.assertTrue("Visual validation of Top Text block is failed", success);
@@ -97,13 +97,12 @@ public class HomePageSteps {
 
     @Then("^then grid elements have the same sizes, not overlapped and aligned in a grid view (\\d+) x (\\d+)$")
     public void then_grid_elements_have_the_same_sizes_not_overlapped_and_aligned_in_a_grid_view(int columns, int rows) {
-        boolean success = uiValidator.init("Validation of a grid view")
+        boolean success = uiValidator.snapshot("Validation of a grid view")
                 .findElements(page.gridElements())
                 .alignedAsGrid(columns, rows)
-                .withSameSize()
-                .areNotOverlappedWithEachOther()
-                .sameTopOffset()
-                .drawMap()
+                .haveEqualSize()
+                .doNotOverlap()
+                .areTopAligned()
                 .validate();
 
         Assert.assertTrue("Visual validation of elements grid view is failed", success);
@@ -115,11 +114,10 @@ public class HomePageSteps {
         for (WebElement card : page.gridElements()) {
             boolean success = uiValidator.init("Validation of style for each of cards in a grid view")
                     .findElement(card.findElement(By.className("project-details")), "Project details block")
-                    .withCssValue("background", hexColor)
-                    .withCssValue("color", "#6f6f6f")
-                    .notOverlapWith(card.findElement(By.className("gallery-hover-4col")), "Image Container")
-                    .sameWidthAs(card.findElement(By.className("gallery-hover-4col")), "Image Container")
-                    .drawMap()
+                    .hasCssValue("background", hexColor)
+                    .hasCssValue("color", "#6f6f6f")
+                    .isNotOverlapping(card.findElement(By.className("gallery-hover-4col")), "Image Container")
+                    .hasEqualWidthAs(card.findElement(By.className("gallery-hover-4col")), "Image Container")
                     .validate();
             softly.assertThat(success);
 
@@ -142,16 +140,14 @@ public class HomePageSteps {
     @Then("^main container and grid are aligned in a center horizontally with equal left and right offset when windows size is (\\d+) x (\\d+)$")
     public void main_container_and_grid_are_aligned_in_a_center_horizontally_with_equal_left_and_right_offset(int width, int height) {
         driver.manage().window().setSize(new Dimension(width, height));
-        boolean successGrid = uiValidator.init("Validation of a grid view")
+        boolean successGrid = uiValidator.snapshot("Validation of a grid view")
                 .findElement(page.gridContainer(), "Grid container")
-                .equalLeftRightOffset()
-                .drawMap()
+                .isCenteredOnPageHorizontally()
                 .validate();
 
         boolean successContainer = uiValidator.init("Validation of a grid view")
                 .findElement(page.mainContainer(), "Main container")
-                .equalLeftRightOffset()
-                .drawMap()
+                .isCenteredOnPageHorizontally()
                 .validate();
 
         Assert.assertTrue("Visual validation of elements grid view and main container is failed", successGrid && successContainer);
@@ -161,16 +157,14 @@ public class HomePageSteps {
     public void main_container_and_grid_are_aligned_in_a_center_horizontally_with_equal_left_and_right_offset_when_zoom_is(int zoom) {
         driver.manage().window().maximize();
         DriverHelper.zoomInOutPage(driver, zoom);
-        boolean successGrid = uiValidator.init("Validation of a grid view with zoom page " + zoom + "%")
+        boolean successGrid = uiValidator.snapshot("Validation of a grid view with zoom page " + zoom + "%")
                 .findElement(page.gridContainer(), "Grid container")
-                .equalLeftRightOffset()
-                .drawMap()
+                .isCenteredOnPageHorizontally()
                 .validate();
 
-        boolean successContainer = uiValidator.init("Validation of a Container view with zoom page " + zoom + "%")
+        boolean successContainer = uiValidator.snapshot("Validation of a Container view with zoom page " + zoom + "%")
                 .findElement(page.mainContainer(), "Main container")
-                .equalLeftRightOffset()
-                .drawMap()
+                .isCenteredOnPageHorizontally()
                 .validate();
 
         Assert.assertTrue("Visual validation of elements grid view and main container is failed", successGrid && successContainer);
